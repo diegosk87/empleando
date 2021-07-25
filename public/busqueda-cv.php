964d3ed8d20941046a8282440e675043778584a6
diegosk87 <diego12345781@hotmail.com>
2021-07-25 16:02:29 -0500

    ✨ Set single cv searcher

---
+++ public/busqueda-cv.php
@@ -0,0 +1,13 @@
+<?php
+require '../vendor/autoload.php';
+require '../config/database.php';
+
+session_start();
+
+if(isset($_SESSION['correo_usuario']) && $_SESSION['tipo_usuario'] == 2) {
+    $empresa = App\Entities\DatosEmpresa::where('correo_empresa', $_SESSION['correo_usuario'])->first();
+    $vacantes = App\Entities\Vacante::where('correo_empresa', $_SESSION['correo_usuario'])->get();
+}
+else header('location:index.php');
+
+include '../resources/views/busqueda-cv-view.php';
